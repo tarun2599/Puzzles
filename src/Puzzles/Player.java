a8c4289d9787e5576cd2dfe08172cab2ba2985ca
Abhimanyu Tyagi <abhimanyu2706@gmail.com>
2022-04-24 00:34:15 +0530

    Accounts_Player_Initial_Commit

---
+++ src/Puzzles/Player.java
@@ -0,0 +1,18 @@
+package Puzzles;
+
+public class Player extends Accounts{
+	
+	public Player(double balance, String name) {
+		super(balance, name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Player(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void check() {}
+	public void raise() {}
+	public void call() {}
+}
