42b83f75685c210a510ecf126430d04fd28ae243
Abhimanyu Tyagi <abhimanyu2706@gmail.com>
2022-04-24 01:07:59 +0530

    Player Second Commit

--- src/Puzzles/Player.java
+++ src/Puzzles/Player.java
@@ -2,6 +2,9 @@ package Puzzles;
 
 public class Player extends Accounts{
 	
+	private double balanceForGame;
+	private double amtKeptAtTable;
+	
 	public Player(double balance, String name) {
 		super(balance, name);
 		// TODO Auto-generated constructor stub
@@ -12,7 +15,34 @@ public class Player extends Accounts{
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void check() {}
-	public void raise() {}
-	public void call() {}
+	/**
+	 * 
+	 * @param amount Buyin Amount
+	 */
+	
+	public void buyIn(double amount) {
+		if(amount > getMaxbuyIn()) 
+			throw new IllegalArgumentException();
+		this.balanceForGame = amount;
+	}
+	
+	public void check() {
+		if(amtKeptAtTable != Dealer.getCurrBet()) {
+			//Can not do this
+		}
+	}
+	public void raise(double amount) {
+		if(amtKeptAtTable != Dealer.getCurrBet()) {
+			//Check if you have enough balance else all in.
+			//Add amount to table = (currBet - amtKeptAtTable) + raise
+			//amtKeptAtTable = currBet  + raise ;
+			//Send msg to dealer to increase currBet
+		}
+	}
+	public void call() {
+		//
+	}
+	public void fold() {
+		//
+	}
 }
--- src/Puzzles/Puzzles.java
+++ src/Puzzles/Puzzles.java
@@ -1,5 +1,14 @@
 package Puzzles;
 
 abstract public class Puzzles {
+	private double maxbuyIn = 1000;
 
+	/**
+	 * @return the maxbuyIn
+	 */
+	public double getMaxbuyIn() {
+		return maxbuyIn;
+	}
+	
+	
 }
