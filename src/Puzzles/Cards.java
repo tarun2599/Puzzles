d562aa70565c95d973fc70d58b15ca2f98fa3cfc
tarun2599 <tarunlakhmani2599@gmail.com>
2022-04-24 14:43:56 +0530

    Create Cards.java

---
+++ src/Puzzles/Cards.java
@@ -0,0 +1,16 @@
+package Puzzles;
+
+public class Cards extends Puzzles
+{
+	private int value;
+	private String suit;
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public String getSuit() {
+		return suit;
+	}
+	
+}
